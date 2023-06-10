046ed72d837d00e1b411aaa64fb55788c33ab131
Gabriela-V-I <gabivanova96@gmail.com>
2023-06-10 08:11:43 +0200

    Add files and update

--- petstagram/petstagram/pets/urls.py
+++ petstagram/petstagram/pets/urls.py
@@ -4,7 +4,7 @@ from petstagram.pets.views import add_page, details_page, edit_page, delete_page
 
 urlpatterns = (
     path('add/', add_page, name='add page'),
-    path('<str:username>/pet/<slug:pet_name>/', include([
+    path('<str:username>/pet/<slug:pet_slug>/', include([
         path('', details_page, name='details page'),
         path('edit/', edit_page, name='edit page'),
         path('delete/', delete_page, name='delete page'),
